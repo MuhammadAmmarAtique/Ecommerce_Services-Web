42fbc896af2470fc04a4c698f7e3f0170af15e7d
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-10-30 22:07:50 +0500

    Designed herosection

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Enabling tooltip for button
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
