7ed7b05fdc62cf8b2e51a46deda43c16fc5b6649
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-11-03 16:30:52 +0500

    added hovering effect on headings using js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
-// Enabling tooltip for button
+//1. Enabling tooltip for button
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
+//2. Changing color of Contact us heading on hovering
+const contactus_heading = document.querySelector('.CS-heading');
+
+// enter
+contactus_heading.addEventListener('mouseenter', () => {
+  contactus_heading.style.color = '#29a9fd';
+});
+// leave
+contactus_heading.addEventListener('mouseleave', () => {
+  contactus_heading.style.color = 'white';
+});
+
+//3. Changing color of About us heading on hovering
+
+const aboutus_heading = document.querySelector('.ASheading');
+
+// enter
+aboutus_heading.addEventListener('mouseenter', () => {
+  aboutus_heading.style.color = '#29a9fd';
+});
+// leave
+aboutus_heading.addEventListener('mouseleave', () => {
+  aboutus_heading.style.color = 'white';
+});
